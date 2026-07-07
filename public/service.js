50544bee7800ed0fb64b6b7076d84623f691ac76
James42020 <james853urron@gmail.com>
2026-07-07 10:17:05 +0100

    removed static resource builder from service.js

--- public/service.js
+++ public/service.js
@@ -1,10 +1,18 @@
 //https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Tutorials/CycleTracker/Service_workers
 
-const html = [
-[ //404
-    "404.html"
-], 
-[ //content
+// The version of the cache.
+// change whenever APP_STATIC_RESOURCES is changed
+const VERSION = "1";
+
+// The name of the cache
+const CACHE_NAME = `otsukare-${VERSION}`;
+
+// The static resources that the app needs to function.
+var APP_STATIC_RESOURCES = [
+    "/",
+
+    "404.html",
+
     "AAAkana.html",
     "AABkanji.html",
     "AACparticles.html",
@@ -14,67 +22,28 @@ const html = [
     "AAGtime.html",
     "AAHpeople.html",
     "AAIthis & that.html",
-    "AAJquestions.html"
-],
-[ //main
+    "AAJquestions.html",
+
     "home.html",
-    "links.html"
-]
-]
-const js = [
+    "links.html",
+
     "index.js",
-    "kana.js"
-]
-const css = [
+    "kana.js",
+
     "index.css",
-    "kana.css"
-]
-const json = [
+    "kana.css",
+
     "dakuon.json",
     "kana.json",
     "special.json",
-    "yoon.json"
-]
-const manifest = [
+    "yoon.json",
+
     "icon-192.png",
     "icon-512.png",
     "icon.svg",
     "manifest.json",
     "service.js"
-]
-
-// The static resources that the app needs to function.
-var APP_STATIC_RESOURCES = [
-    "/"
 ];
-
-function setUpStatic(){
-    for(i=0;i<html.length;i++){
-        for(j=0;j<html[i].length;j++){
-            APP_STATIC_RESOURCES.push(`/html/${html[i][j]}`)
-        }
-    }
-    for(i=0;i<js.length;i++){
-        APP_STATIC_RESOURCES.push(`/js/${js[i]}`)
-    }
-    for(i=0;i<css.length;i++){
-        APP_STATIC_RESOURCES.push(`/css/${css[i]}`)
-    }
-    for(i=0;i<json.length;i++){
-        APP_STATIC_RESOURCES.push(`/json/${json[i]}`)
-    }
-    for(i=0;i<manifest.length;i++){
-        APP_STATIC_RESOURCES.push(`/manifest/${manifest[i]}`)
-    }
-}
-setUpStatic()
-
-// The version of the cache.
-// change whenever APP_STATIC_RESOURCES is changed
-const VERSION = "1";
-
-// The name of the cache
-const CACHE_NAME = `otsukare-${VERSION}`;
 
 // On install, cache the static resources
 self.addEventListener("install", (event) => {
